e21cae52b9ce0b3d9293a3d59e5aeee4c017a70c
crimro.se <crimrosegit@proton.me>
2024-12-12 13:04:46 +0000

    main is now an experiment using the new processor

--- main.go
+++ main.go
@@ -3,21 +3,15 @@ package main
 import (
 	"context"
 	"fmt"
-	"image"
-	"image/color"
-	"image/jpeg"
-	"image/png"
-	"io"
-	"io/fs"
 	"strings"
+	"time"
 
 	archivewalk "github.com/crimro-se/imagedb/archivewalk"
-	"github.com/crimro-se/imagedb/imageutil"
-	"golang.org/x/image/webp"
 )
 
 func main() {
 	ctx := context.Background()
+	dbfilename := "file.sqlite"
 	errCh := make(chan error, 5)
 	defer close(errCh)
 	go func() {
@@ -26,52 +20,22 @@ func main() {
 		}
 	}()
 
-	aw := archivewalk.NewArchiveWalker(10, errCh, true, true, imagehandler)
+	// create db if needed
+	db, err := NewDatabase(dbfilename, true)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	db.Close()
+	ip, err := NewImageProcessor(dbfilename, 0)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	aw := archivewalk.NewArchiveWalker(10, errCh, true, true, ip.Handler)
 	aw.Walk("test_data/valid", ctx)
-}
-
-// 1. reads pre-processed image data from a channel,
-// 2. obtains embedding,
-// 3. updates database
-func embeddingWorker() {}
-
-// callback function for archivewalk,
-// loads and resizes images, sends data to a channel for further processing afterwards.
-func imagehandler(path, vpath string, file io.Reader, d fs.DirEntry, threadID int) error {
-	ext := ""
-	if len(vpath) > 0 {
-		ext = getExt(vpath)
-	} else {
-		ext = getExt(path)
-	}
-	var err error
-
-	// TODO: db checks based on this (info.ModTime)
-	// might need another channel for requesting and checking this data.
-	info, err := d.Info()
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-	_ = info.ModTime()
-
-	var img image.Image
-	switch ext {
-	case "jpg":
-		img, err = jpeg.Decode(file)
-	case "png":
-		img, err = png.Decode(file)
-	case "webp":
-		img, err = webp.Decode(file)
-	default:
-		return nil
-	}
-	if err != nil {
-		fmt.Println(err)
-	}
-	imageutil.ScaleImagePaddedSquareRGBA(img, color.RGBA{255, 255, 255, 255}, 256)
-
-	return nil
+	time.Sleep(1 * time.Second)
 }
 
 // returns the file extension in lower-case.
